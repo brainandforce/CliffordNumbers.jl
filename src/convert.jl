677a6b93a5a45327798c27ea9804bb284d933321
Brandon Flores <bsflores@wisc.edu>
2023-02-10 15:13:44 -0600

    Added scalar conversion for `CliffordNumber` (to `Real`)

--- src/convert.jl
+++ src/convert.jl
@@ -1,3 +1,8 @@
 import Base.convert
 
 convert(S::Type{<:CliffordNumber{Q,T}}, m::CliffordNumber{Q}) where {Q,T} = S(m.data)::S
+
+function convert(S::Type{<:Real}, m::CliffordNumber)
+    isscalar(m) || throw(InexactError(:convert, S, m))
+    return S(m[0])
+end
