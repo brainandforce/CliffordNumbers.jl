2f943ef3f0eaac9dba2a9c6e05e32df3643d5518
Brandon Flores <bsflores@wisc.edu>
2023-03-06 13:34:30 -0600

    Greatly simplified promotion logic

--- src/promote.jl
+++ src/promote.jl
@@ -12,23 +12,16 @@ promote_eltypeof() = Base.Bottom
 promote_eltypeof(x, y...) = promote_type(eltypeof(x), promote_eltypeof(y...))
 
 # Generic promote rule for Clifford numbers with different element types
-function promote_rule(S::Type{<:CliffordNumber{Q}}, T::Type{<:CliffordNumber{Q}}) where Q
+function promote_rule(
+    S::Type{<:AbstractCliffordNumber{Q}},
+    T::Type{<:AbstractCliffordNumber{Q}}
+) where Q
     return CliffordNumber{Q,promote_eltypeof(S,T),elements(Q)}
 end
 
-# Promote rule when using real numbers
-function promote_rule(S::Type{<:CliffordNumber{Q,<:Real}}, T::Type{<:Real}) where Q
+# Promote rule for BaseNumber types (real, complex)
+# Note that complex numbers aren't automatically treated as pseudoscalars
+# (this only works in some dimensions...)
+function promote_rule(S::Type{<:AbstractCliffordNumber{Q}}, T::Type{<:BaseNumber}) where Q
     return CliffordNumber{Q,promote_eltypeof(S,T),elements(Q)}
 end
-
-# Promote rule for complex numbers with real Clifford numbers
-function promote_rule(S::Type{<:CliffordNumber{Q,<:Real}}, T::Type{<:Complex{R}}) where {Q,R}
-    return CliffordNumber{Q,promote_eltypeof(S,R),elements(Q)}
-end
-
-# For abstract types where no element type of a Clifford number is specified
-promote_rule(::Type{CliffordNumber{Q}}, S::Type{<:CliffordNumber{Q,T}}) where {Q,T} = S
-promote_rule(::Type{CliffordNumber{Q}}, ::Type{T}) where {Q,T<:Real} = CliffordNumber{Q,T}
-
-# TODO: may need macros to handle the case of a CliffordNumber{Cl} being promoted with a Complex
-# In that case, promotion depends on whether the pseudoscalar behaves like the imaginary unit
