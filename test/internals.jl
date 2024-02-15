4896ff0251a675a4babec9f067f8468d3706275d
Brandon Flores <bsflores@wisc.edu>
2024-02-15 13:33:44 -0600

    Added tests for Hamming weight tools

--- test/internals.jl
+++ test/internals.jl
@@ -9,3 +9,17 @@
     # Should always pass unless we specialize on integers
     @test all(CliffordNumbers.intlog2(n) === CliffordNumbers.intlog2(Float64(n)) for n in 0:65535)
 end
+
+@testset "Hamming weight tools" begin
+    import CliffordNumbers: isevil, isodious, evil_number, odious_number, hamming_number
+    # Sequences pulled from OEIS
+    first_16_evil = [0, 3, 5, 6, 9, 10, 12, 15, 17, 18, 20, 23, 24, 27, 29, 30]
+    first_16_odious = [1, 2, 4, 7, 8, 11, 13, 14, 16, 19, 21, 22, 25, 26, 28, 31]
+    hamming_weight_3 = [7, 11, 13, 14, 19, 21, 22, 25, 26, 28, 35, 37, 38, 41, 42, 44]
+    @test all(isevil, first_16_evil)
+    @test all(isodious, first_16_odious)
+    @test evil_number.(1:16) == first_16_evil
+    @test odious_number.(1:16) == first_16_odious
+    @test all(x -> count_ones(x) == 3, hamming_weight_3)
+    @test hamming_number.(3, 1:16) == hamming_weight_3
+end
