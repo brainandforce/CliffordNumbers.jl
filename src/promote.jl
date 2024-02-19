28bec208f38e38109686d274b64585bcd1814844
Brandon Flores <bsflores@wisc.edu>
2024-02-19 02:37:33 -0600

    Fixed promote rules

--- src/promote.jl
+++ src/promote.jl
@@ -1,27 +1,29 @@
 import Base.promote_rule
 
-# This set of functions is defined in Julia Base (abstractarray.jl), but rather than importing it
-# we're duplicating it (this part of the Julia API may be unstable...)
-promote_eltype() = Base.Bottom
-promote_eltype(x, y...) = promote_type(eltype(x), promote_eltype(y...))
+# Inspired by Base.promote_eltype in Julia Base
+"""
+    promote_numeric_type(x, y)
 
-eltypeof(x) = typeof(x)
-eltypeof(x::AbstractCliffordNumber) = eltype(x)
+Calls `promote_type()` for the result of `numeric_type()` called on all arguments. For incompletely
+specified types, the result of `numeric_type()` is replaced with `Bool`, which always promotes to
+any larger numeric type.
+"""
+promote_numeric_type() = Union{}
 
-promote_eltypeof() = Base.Bottom
-promote_eltypeof(x, y...) = promote_type(eltypeof(x), promote_eltypeof(y...))
+function promote_numeric_type(x, y...)
+    T = numeric_type(x)
+    return promote_type(ifelse(T >: BaseNumber, Bool, T), promote_numeric_type(y...))
+end
 
 # Generic promote rule for Clifford numbers with different element types
 function promote_rule(
     S::Type{<:AbstractCliffordNumber{Q}},
     T::Type{<:AbstractCliffordNumber{Q}}
 ) where Q
-    return CliffordNumber{Q,promote_eltype(S,T),elements(Q)}
+    return CliffordNumber{Q,promote_numeric_type(S,T),elements(Q)}
 end
 
 # Promote rule for BaseNumber types (real, complex)
 # Note that complex numbers aren't automatically treated as pseudoscalars
 # (this only works in some dimensions...)
-function promote_rule(S::Type{<:AbstractCliffordNumber{Q}}, T::Type{<:BaseNumber}) where Q
-    return CliffordNumber{Q,promote_type(eltype(S),T),elements(Q)}
-end
+promote_rule(C::Type{<:AbstractCliffordNumber}, N::Type{<:BaseNumber}) = similar_type(C, N)
