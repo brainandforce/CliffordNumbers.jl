733a20ce940f622226813727ae7e236ac770433c
Brandon Flores <bsflores@wisc.edu>
2024-02-19 18:35:28 -0600

    Added checks in default `AbstractCliffordNumber` conversion

--- src/convert.jl
+++ src/convert.jl
@@ -1,6 +1,10 @@
 import Base.convert
 
-convert(T::Type{<:AbstractCliffordNumber{Q}}, x::AbstractCliffordNumber{Q}) where Q = T(x)::T
+# Default conversion should check for exact representability
+function convert(T::Type{<:AbstractCliffordNumber{Q}}, x::AbstractCliffordNumber{Q}) where Q
+    has_grades_of(x, T) && return T(x)
+    throw(InexactError(:convert, T, x))
+end
 
 function convert(S::Type{<:Real}, x::CliffordNumber)
     isscalar(x) || throw(InexactError(:convert, S, x))
