58c81e74cb6052c88405522e80a67080efc8e16e
Brandon Flores <bsflores@wisc.edu>
2024-02-23 15:52:49 -0600

    Added promotion rules for `Z2CliffordNumber`

--- src/promote.jl
+++ src/promote.jl
@@ -16,6 +16,12 @@ function promote_numeric_type(x, y...)
 end
 
 # Generic promote rule for Clifford numbers with different element types
+#= TODO:
+    This definition can screw things up sometimes with promote_type, because it evaluates
+    promote_rule in both orders and then promotes the result of both. In the case of KVector and
+    Z2CliffordNumber, both orders need to be defined or the result is always CliffordNumber (it 
+    should be Z2CliffordNumber if the KVector matches the parity of the Z2CliffordNumber)
+=#
 function promote_rule(
     ::Type{<:AbstractCliffordNumber{Q,S}},
     ::Type{<:AbstractCliffordNumber{Q,T}}
@@ -37,6 +43,17 @@ function promote_rule(::Type{<:KVector{K,Q,S}}, ::Type{<:KVector{K,Q,T}}) where 
     return KVector{K,Q,promote_type(S,T),binomial(dimension(Q),K)}
 end
 
+@generated function promote_rule(
+    ::Type{<:KVector{K1,Q,T1}},
+    ::Type{<:KVector{K2,Q,T2}}
+) where {K1,K2,Q,T1,T2}
+    if xor(iseven(K1), iseven(K2))
+        return :(CliffordNumber{Q,promote_type(T1,T2),elements(Q)})
+    else
+        return :(Z2CliffordNumber{iseven(K1),Q,promote_type(T1,T2),div(elements(Q),2)})
+    end
+end
+
 function promote_rule(::Type{<:KVector{<:Any,Q,T}}, ::Type{N}) where {Q,T,N<:BaseNumber}
     return CliffordNumber{Q,promote_type(T,N),elements(Q)}
 end
@@ -45,3 +62,27 @@ end
 function promote_rule(::Type{<:KVector{0,Q,T}}, ::Type{N}) where {Q,T,N<:BaseNumber}
     return KVector{0,Q,promote_type(T,N),1}
 end
+
+function promote_rule(
+    ::Type{<:Z2CliffordNumber{P,Q,S}},
+    ::Type{<:Z2CliffordNumber{P,Q,T}}
+) where {P,Q,S,T}
+    return Z2CliffordNumber{P,Q,promote_type(S,T)}
+end
+
+@generated function promote_rule(
+    ::Type{<:Z2CliffordNumber{P,Q,S}},
+    ::Type{<:KVector{K,Q,T}}
+) where {P,K,Q,S,T}
+    if xor(P, isodd(K))
+        return :(CliffordNumber{Q,promote_type(S,T),elements(Q)})
+    else
+        return :(Z2CliffordNumber{P,Q,promote_type(S,T),div(elements(Q), 2)})
+    end
+end
+
+#= TODO:
+    This needs to be defined because of how Base.promote_type is implemented and how the Default
+    type promotion for CliffordNumber subtypes is defined.
+=#
+promote_rule(C1::Type{<:KVector}, C2::Type{<:Z2CliffordNumber}) = promote_rule(C2, C1)
