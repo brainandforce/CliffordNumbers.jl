1655c79c6ae35a8dd145f1effbbfdc8365a6fe48
Brandon Flores <bsflores@wisc.edu>
2024-03-12 02:25:00 -0500

    Tweaked kernel signature slightly, added notes on performance

--- src/multiply.jl
+++ src/multiply.jl
@@ -7,11 +7,20 @@ function _ndmult(a::BitIndex{Q}, B::NTuple{L,BitIndex{Q}}) where {L,Q}
     return map(b -> nondegenerate_mult(a, b), B)
 end
 
+#=
+    Specialized kernel for multiplying two CliffordNumber or Z2CliffordNumber instances
+    
+    NOTE: this is much faster if the arguments are the same type
+    (CliffordNumber or Z2CliffordNumber; parity is irrelevant for the latter)
+    TODO: handle the case where the smaller argument comes first
+
+    NOTE: this is much faster if the first input is smaller than the second input
+=#
 @generated function mul(
     ::Type{C},
-    x::T,
-    y::T,
-) where {Q,C<:AbstractCliffordNumber{Q},T<:AbstractCliffordNumber{Q}}
+    x::Union{CliffordNumber{Q,T},Z2CliffordNumber{<:Any,Q,T}},
+    y::Union{CliffordNumber{Q,T},Z2CliffordNumber{<:Any,Q,T}}
+) where {Q,T,C<:AbstractCliffordNumber{Q,T}}
     BC = Tuple(BitIndices(C))
     z = zero_tuple(C)
     ex = :($z)
