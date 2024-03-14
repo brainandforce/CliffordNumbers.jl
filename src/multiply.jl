20f94b99c4f5119659261689be527beda77c49a3
Brandon Flores <bsflores@wisc.edu>
2024-03-13 23:00:33 -0500

    Added grade filtering machinery to multiply kernel

--- src/multiply.jl
+++ src/multiply.jl
@@ -19,7 +19,7 @@ end
 end
 
 bitindex_shuffle(a::BitIndex{Q}, B::BitIndices{Q}) where Q = bitindex_shuffle(a, Tuple(B))
-bitindex_shuffle( B::BitIndices{Q}, a::BitIndex{Q}) where Q = bitindex_shuffle(Tuple(B), a)
+bitindex_shuffle(B::BitIndices{Q}, a::BitIndex{Q}) where Q = bitindex_shuffle(Tuple(B), a)
 
 """
     CliffordNumbers.nondegenerate_mask(a::BitIndex{Q}, B::NTuple{L,BitIndex{Q}})
@@ -80,6 +80,28 @@ end
 
 const ContractionGradeFilters = Union{GradeFilter{:⨼},GradeFilter{:⨽},GradeFilter{:dot}}
 
+"""
+    CliffordNumbers.mul_mask(F::GradeFilter, a::BitIndex{Q}, B::NTuple{L,BitIndices{Q}})
+    CliffordNumbers.mul_mask(F::GradeFilter, B::NTuple{L,BitIndices{Q}}, a::BitIndex{Q})
+
+    CliffordNumbers.mul_mask(F::GradeFilter, a::BitIndex{Q}, B::BitIndices{Q})
+    CliffordNumbers.mul_mask(F::GradeFilter, B::BitIndices{Q}, a::BitIndex{Q})
+
+Generates a `NTuple{L,Bool}` which is `true` whenever the multiplication of the blade indexed by `a`
+and blades indexed by `B` is nonzero. `false` is returned if the grades multiply to zero due to the
+squaring of a degenerate component, or if they are filtered by `F`.
+"""
+function mul_mask(F::GradeFilter, a::BitIndex{Q}, B::NTuple{L,BitIndex{Q}}) where {L,Q}
+    return map(b -> F(a,b) & nondegenerate_mult(a,b), B)
+end
+
+function mul_mask(F::GradeFilter, B::NTuple{L,BitIndex{Q}}, a::BitIndex{Q}) where {L,Q}
+    return map(b -> F(b,a) & nondegenerate_mult(b,a), B)
+end
+
+mul_mask(F::GradeFilter, a::BitIndex{Q}, B::BitIndices{Q}) where Q = mul_mask(F, a, Tuple(B))
+mul_mask(F::GradeFilter, B::BitIndices{Q}, a::BitIndex{Q}) where Q = mul_mask(F, Tuple(B), a)
+
 #---Product return types---------------------------------------------------------------------------#
 """
     CliffordNumbers.product_return_type(::Type{X}, ::Type{Y}, [::GradeFilter{S}])
@@ -139,25 +161,6 @@ end
 
 Returns the type of the result of the geometric product of the input types.
 """
-#=
-@generated function geometric_product_type(
-    ::Type{C1},
-    ::Type{C2}
-) where {Q,C1<:AbstractCliffordNumber{Q},C2<:AbstractCliffordNumber{Q}}
-    c1_odd = all(isodd, nonzero_grades(C1))
-    c2_odd = all(isodd, nonzero_grades(C2))
-    c1_even = all(iseven, nonzero_grades(C1))
-    c2_even = all(iseven, nonzero_grades(C2))
-    P = (c1_odd && c2_even) || (c1_even && c2_odd)
-    T = promote_numeric_type(C1,C2)
-    if (!c1_odd && !c1_even) || (!c2_odd && !c2_even)
-        return :(CliffordNumber{Q,$T,elements(Q)})
-    else
-        return :(Z2CliffordNumber{$P,Q,$T,div(elements(Q), 2)})
-    end
-end
-=#
-
 function geometric_product_type(
     ::Type{C1},
     ::Type{C2}
@@ -166,7 +169,11 @@ function geometric_product_type(
 end
 
 """
-    CliffordNumbers.mul(x::AbstractCliffordNumber{Q,T}, y::AbstractCliffordNumber{Q,T})
+    CliffordNumbers.mul(
+        x::Union{CliffordNumber{Q,T},Z2CliffordNumber{<:Any,Q,T}},
+        y::Union{CliffordNumber{Q,T},Z2CliffordNumber{<:Any,Q,T}},
+        [F::GradeFilter = GradeFilter{:*}()]
+    )
 
 A fast geometric product implementation using generated functions for specific cases, and generic
 methods which either convert the arguments or fall back to other methods.
@@ -185,16 +192,14 @@ multiplications this may be best solved by simply converting KVector arguments u
 """
 @generated function mul(
     x::Union{CliffordNumber{Q,T},Z2CliffordNumber{<:Any,Q,T}},
-    y::Union{CliffordNumber{Q,T},Z2CliffordNumber{<:Any,Q,T}}
+    y::Union{CliffordNumber{Q,T},Z2CliffordNumber{<:Any,Q,T}},
+    F::GradeFilter = GradeFilter{:*}()
 ) where {Q,T}
-    C = geometric_product_type(x, y)
-    # Construct the 
-    BC = Tuple(BitIndices(C))
-    z = zero_tuple(C)
-    ex = :($z)
+    C = product_return_type(x, y, F())
+    ex = :($(zero_tuple(C)))
     for a in BitIndices(x)
-        inds = bitindex_shuffle(a, BC)
-        mask = nondegenerate_mask(a, BC)
+        inds = bitindex_shuffle(a, Tuple(BitIndices(C)))
+        mask = mul_mask(F(), a, Tuple(BitIndices(C)))
         ex = :(map(muladd, x[$a] .* $mask, y[$inds], $ex))
     end
     return :($C($ex))
