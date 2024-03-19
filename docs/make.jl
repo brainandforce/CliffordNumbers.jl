797704974baad41905b6ff19dab64a9a02aa586f
Brandon Flores <bsflores@wisc.edu>
2024-03-19 15:03:55 -0500

    Improved documentation building instructions

--- docs/make.jl
+++ docs/make.jl
@@ -3,19 +3,22 @@ using Documenter
 
 DocMeta.setdocmeta!(CliffordNumbers, :DocTestSetup, :(using CliffordNumbers); recursive=true)
 
+is_ci_env = (get(ENV, "CI", nothing) == true)
+@info "is_ci_env == $is_ci_env"
+
 makedocs(;
-    modules=[CliffordNumbers],
-    authors="Brandon Flores",
-    repo="https://github.com/brainandforce/CliffordNumbers.jl/blob/{commit}{path}#{line}",
     sitename="CliffordNumbers.jl",
+    modules=[CliffordNumbers],
+    checkdocs = :exports,
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://brainandforce.github.io/CliffordNumbers.jl",
-        edit_link="main",
-        assets=String[],
+        prettyurls = is_ci_env,
+        canonical = "https://brainandforce.github.io/CliffordNumbers.jl",
+        edit_link = "main",
+        assets = String[],
     ),
     pages=[
         "Home" => "index.md",
+        "API" => "api.md"
     ],
 )
 
