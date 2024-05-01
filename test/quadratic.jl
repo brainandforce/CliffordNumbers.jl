3686d334c5db4408c7031da6fb5133cfd86b9768
Brandon Flores <bsflores@wisc.edu>
2024-05-01 16:37:07 -0500

    Fixed `QuadraticForms` tests

--- test/quadratic.jl
+++ test/quadratic.jl
@@ -1,7 +1,7 @@
 @testset "Quadratic forms" begin
     @test dimension(QuadraticForm{3,2,1}) === 6
-    @test CliffordNumbers.isdegenerate(QuadraticForm{3,2,1})
-    @test !CliffordNumbers.isdegenerate(QuadraticForm{3,2,0})
-    @test !CliffordNumbers.iseuclidean(QuadraticForm{3,2,0})
-    @test CliffordNumbers.iseuclidean(QuadraticForm{3,0,0})
+    @test CliffordNumbers.is_degenerate(QuadraticForm{3,2,1})
+    @test !CliffordNumbers.is_degenerate(QuadraticForm{3,2,0})
+    @test !CliffordNumbers.is_positive_definite(QuadraticForm{3,2,0})
+    @test CliffordNumbers.is_positive_definite(QuadraticForm{3,0,0})
 end
