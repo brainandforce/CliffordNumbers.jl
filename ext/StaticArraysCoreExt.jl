17108c71ef6fd5632f048a90986c12e8304f6a55
Brandon Flores <bsflores@wisc.edu>
2024-06-04 14:13:52 -0500

    Added extension to support `StaticArraysCore.similar_type`

---
+++ ext/StaticArraysCoreExt.jl
@@ -0,0 +1,14 @@
+module StaticArraysCoreExt
+
+using CliffordNumbers
+using StaticArraysCore
+
+function StaticArraysCore.similar_type(::Type{C}, args...) where C<:AbstractCliffordNumber
+    return CliffordNumbers.similar_type(C, args...)
+end
+
+function StaticArraysCore.similar_type(x::AbstractCliffordNumber, args...) 
+    return CliffordNumbers.similar_type(x, args...)
+end
+
+end
