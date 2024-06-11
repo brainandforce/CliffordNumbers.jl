6ffb37f950bd8ff2fceeb476049f3c64ede109d7
Brandon Flores <bsflores@wisc.edu>
2024-06-11 17:16:46 -0500

    Used `@basis_vars` in tests for defining basis 1-blades

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,19 +4,9 @@ using Aqua, Test
 Aqua.test_all(CliffordNumbers; unbound_args = false)
 
 # Define basis vectors for important algebras
-const σ1 = KVector{1,VGA(3)}(1, 0, 0)
-const σ2 = KVector{1,VGA(3)}(0, 1, 0)
-const σ3 = KVector{1,VGA(3)}(0, 0, 1)
-
-const e0 = KVector{1,PGA(3)}(1, 0, 0, 0)
-const e1 = KVector{1,PGA(3)}(0, 1, 0, 0)
-const e2 = KVector{1,PGA(3)}(0, 0, 1, 0)
-const e3 = KVector{1,PGA(3)}(0, 0, 0, 1)
-
-const γ0 = KVector{1,STA}(1, 0, 0, 0)
-const γ1 = KVector{1,STA}(0, 1, 0, 0)
-const γ2 = KVector{1,STA}(0, 0, 1, 0)
-const γ3 = KVector{1,STA}(0, 0, 0, 1)
+@basis_vars(VGA(3), Int, 'σ')
+@basis_vars(PGA(3), Int)
+@basis_vars(STA)
 
 @testset "CliffordNumbers.jl" begin
     include("internals.jl")
