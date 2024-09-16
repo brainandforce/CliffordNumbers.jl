5747966f7f6e7fd4216af321eececfd10f386d4b
Brandon Flores <bsflores@wisc.edu>
2024-09-16 15:51:55 -0500

    Added generic `rrule` for `AbstractCliffordNumber`

--- ext/CliffordNumbersChainRulesCoreExt.jl
+++ ext/CliffordNumbersChainRulesCoreExt.jl
@@ -2,4 +2,15 @@ module CliffordNumbersChainRulesCoreExt
 
 using CliffordNumbers
 
+import ChainRulesCore: NoTangent, ProjectTo, frule, rrule
+
+# Generic rrule for all AbstractCliffordNumber instances
+# TODO: check that this works for a variety of cases
+# It does not seem to get gradients correct for f(x) = x^2
+# But it does seem to work for f(x) = x*x
+function rrule(::Type{T}, t::Tuple) where T<:AbstractCliffordNumber
+    CliffordNumber_pullback(∇x) = (NoTangent(), Tuple(∇x))
+    return (T(t), CliffordNumber_pullback)
+end
+
 end
