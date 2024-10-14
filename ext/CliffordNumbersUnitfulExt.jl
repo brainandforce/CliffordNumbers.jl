9f8eb8cf3fd6934e5210eadfb97a82e59071c05f
Brandon Flores <bsflores@wisc.edu>
2024-10-14 18:03:17 -0500

    Added wedge product definitions

--- ext/CliffordNumbersUnitfulExt.jl
+++ ext/CliffordNumbersUnitfulExt.jl
@@ -2,6 +2,22 @@ module CliffordNumbersUnitfulExt
 
 using CliffordNumbers
 using Unitful
-import CliffordNumbers: ∧
+
+import CliffordNumbers: ∧, ∨, ⨼, ⨽, dot, ×, ⨰
+import Unitful: AbstractQuantity, AffineError, AffineQuantity
+
+∧(x::AbstractQuantity, y::AbstractQuantity) = Quantity(x.val ∧ y.val, unit(x) * unit(y))
+
+function ∧(x::Number, y::AbstractQuantity)
+    y isa AffineQuantity &&
+        throw(AffineError("an invalid operation was attempted with affine quantities: $x ∧ $y"))
+    return Quantity(x ∧ y.val, unit(y))
+end
+
+function ∧(x::AbstractQuantity, y::Number)
+    x isa AffineQuantity &&
+        throw(AffineError("an invalid operation was attempted with affine quantities: $x ∧ $y"))
+    return Quantity(x.val ∧ y, unit(x))
+end
 
 end
