b893ccca3f561d947f7066c478cb0313dcd43577
Brandon Flores <bsflores@wisc.edu>
2024-10-17 20:20:16 -0500

    Throw errors for invalid construction of unitful Clifford numbers

--- test/ext/Unitful.jl
+++ test/ext/Unitful.jl
@@ -1,6 +1,8 @@
 @testset "Unitful extension" begin
     x = KVector{1,VGA(3)}(1, 0, 0)
     y = KVector{1,VGA(3)}(0, 1, 0)
+    @test_throws ArgumentError KVector{1,VGA(3)}(1u"m", 0u"m", 0u"m")
+    @test_throws ArgumentError KVector{1,VGA(3)}(0u"m", 1u"m/s", 0u"K")
     xu = x*u"m"
     yu = y*u"m"
     @test xu ∧ yu === KVector{2,VGA(3)}(1, 0, 0) * u"m^2"
