f1f49a4cc3e147379fcf4ef8c1f1143b2052debf
Brandon Flores <bsflores@wisc.edu>
2024-11-04 18:46:43 -0600

    Made implementations of reverse, grade involution, and Clifford conjugate for `KVector` simpler

--- src/math/duals.jl
+++ src/math/duals.jl
@@ -7,9 +7,9 @@ end
 reverse(x::AbstractCliffordNumber) = adjoint(x)
 
 # Faster implementations for KVector that don't require indexing
-adjoint(x::T) where T<:KVector = T(x.data .* Int8(-1)^!iszero(grade(x) & 2))
-grade_involution(x::T) where T<:KVector = T(x.data .* Int8(-1)^isodd(grade(x)))
-conj(x::T) where T<:KVector = T(x.data .* Int8(-1)^!iszero((grade(x) + 1) & 2))
+adjoint(x::KVector{K}) where K = ifelse(iszero(K & 2), x, -x)
+grade_involution(x::KVector{K}) where K = ifelse(iseven(K), x, -x)
+conj(x::KVector{K}) where K = ifelse(iszero((K + 1) & 2), x, -x)
 
 left_complement(x::AbstractCliffordNumber) = x[right_complement.(BitIndices(complement_type(x)))]
 right_complement(x::AbstractCliffordNumber) = x[left_complement.(BitIndices(complement_type(x)))]
