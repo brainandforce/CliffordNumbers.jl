2c115c479ec95afb59cb9cddd3187f2a34f8936f
Brandon Flores <bsflores@wisc.edu>
2024-11-11 14:50:53 -0600

    Added missing test for `similar_type` method

--- test/ext/StaticArraysCore.jl
+++ test/ext/StaticArraysCore.jl
@@ -1,4 +1,6 @@
 @testset "StaticArraysCore extension" begin
     k = KVector{1,VGA(3)}(4, 2, 0)
+    T = typeof(k)
     @test CliffordNumbers.similar_type(k, Float32) === StaticArrays.similar_type(k, Float32)
+    @test CliffordNumbers.similar_type(T, Float32) === StaticArrays.similar_type(T, Float32)
 end
