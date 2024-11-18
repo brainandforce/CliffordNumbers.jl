d38cc700a6eff0a4c23ad7d1e30fd4d3123d142c
Brandon Flores <bsflores@wisc.edu>
2024-11-18 17:52:23 -0600

    Default to `Float64` scalar types if not specified

--- src/random.jl
+++ src/random.jl
@@ -7,6 +7,11 @@ function randexp(rng::AbstractRNG, ::SamplerType{<:KVector{0,Q,T}}) where {Q,T}
     return KVector{0,Q}(randexp(rng, T))
 end
 
+# Default to Float64 scalars
+rand(rng::AbstractRNG, ::SamplerType{KVector{0,Q}}) where Q = KVector{0,Q}(rand(rng, Float64))
+randn(rng::AbstractRNG, ::SamplerType{KVector{0,Q}}) where Q = KVector{0,Q}(randn(rng, Float64))
+randexp(rng::AbstractRNG, ::SamplerType{KVector{0,Q}}) where Q = KVector{0,Q}(randexp(rng, Float64))
+
 #---Random sampling of unit 1-blades---------------------------------------------------------------#
 """
     randn([rng=default_rng()], ::Type{KVector{1,Q,T}})
@@ -22,4 +27,9 @@ the [chi distribution], and their moduli distributed according to the [chi-squar
 """
 function randn(rng::AbstractRNG, ::SamplerType{<:KVector{1,Q,T}}) where {Q,T}
     return KVector{K,Q}(ntuple(_ -> randn(rng, T), Val(dimension(Q))))
+end
+
+# Default to Float64 scalars
+function randn(rng::AbstractRNG, ::SamplerType{KVector{1,Q}}) where Q
+    return KVector{K,Q}(ntuple(_ -> randn(rng, Float64), Val(dimension(Q))))
 end
